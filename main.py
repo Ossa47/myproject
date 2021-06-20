fed85eee2b2fb5a9e049b69df1f864e34f57476d
Ossa47 <83310115+Ossa47@users.noreply.github.com>
2021-06-20 14:18:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from kivymd.app import MDApp
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.screenmanager import Screen, ScreenManager
-from funcs import Encrypt
+from Caesar_funcs import Encrypt
 
 
 
 class Caesar_Screen(Screen):
     pass
-
-class Aes_Screen(Screen):
-    pass
-
-sm = ScreenManager()
-sm.add_widget(Caesar_Screen(name='Caesar'))
-sm.add_widget(Aes_Screen(name='Aes'))
 
 
 class CryptoMD(MDApp):
@@ -31,7 +22,7 @@ class CryptoMD(MDApp):
         key = key
         Key = int(key)
         out_encrypted = obj.encrypt(Text, key)
-        self.root.ids.screen_manager.get_screen("Caesar_Screen").ids.output.text = out_encrypted
+        self.root.get_screen("Caesar_Screen").ids.output.text = out_encrypted
 
     def out_decrypt(self, Text, key):
         obj = Encrypt()
@@ -39,7 +30,7 @@ class CryptoMD(MDApp):
         key = key
         Key = int(key)
         out_decrypted = obj.decrypt(Text, key)
-        self.root.Caesar.ids.output.text = out_decrypted
+        self.root.get_screen("Caesar_Screen").ids.output.text = out_decrypted
 
 
 if __name__ == '__main__':
